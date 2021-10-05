3402a73c25e674ed3cf7e6934f85236eb833b69a
damA <lmollonb@iesch.org>
2021-10-05 14:58:29 +0200

    all

--- 1Trimestre/5-ResgistroDeSocios/app/src/main/java/com/laura/myapplication/MainActivity.java
+++ 1Trimestre/5-ResgistroDeSocios/app/src/main/java/com/laura/myapplication/MainActivity.java
@@ -18,15 +18,37 @@ public class MainActivity extends AppCompatActivity {
         Button btnGuardar = findViewById(R.id.boton_guardar);
         EditText etNombre= findViewById(R.id.nombre_socio);
         EditText etApellidos = findViewById(R.id.apellido_socio);
+        EditText etBio = findViewById(R.id.editTextBio);
         RatingBar rtBar= findViewById(R.id.ratingBar);
 
 
         btnGuardar.setOnClickListener(view -> {
             String nombreSocio = etNombre.getText().toString();
-            String apellidosSocio=etApellidos.getText().toString();
+            String apellidoSocio=etApellidos.getText().toString();
+            String bioSocio = etBio.getText().toString();
             Float puntuacion=rtBar.getRating();
 
+            //1-Nos creamos el intent irDetalle
             Intent irDetalle = new Intent(this, DetailActivity.class);
+
+            //2a-Añadimos toda la informacion que queremos pasar al detalle activity
+                // irDetalle.putExtra("nombreSocio",nombreSocio);
+                //irDetalle.putExtra("apellidoSocio",apellidoSocio);
+                //irDetalle.putExtra("bioSocio",bioSocio);
+                //irDetalle.putExtra("puntuacion",puntuacion);
+
+            //2b.1-Creamos un objeto socio con los datos que corresponden
+            Socio nuevoSocio = new Socio(
+                    nombreSocio,
+                    apellidoSocio,
+                    bioSocio,
+                    puntuacion
+            );
+
+            //2b.2 Enviamos el objeto al intent
+            irDetalle.putExtra("nuevoSocio",nuevoSocio);
+
+            //3-Nos vamos al detail activity
             startActivity(irDetalle);
 
         });
