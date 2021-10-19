824c28f71fd1913399f4ca9fb4682457b03f3a2e
damA <lmollonb@iesch.org>
2021-10-19 16:11:54 +0200

    dashboard

---
+++ 1Trimestre/6DashBoard/app/src/main/java/com/laura/a6dashboard/activities/Settings_Activity.java
@@ -0,0 +1,16 @@
+package com.laura.a6dashboard.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.laura.a6dashboard.R;
+
+public class Settings_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
