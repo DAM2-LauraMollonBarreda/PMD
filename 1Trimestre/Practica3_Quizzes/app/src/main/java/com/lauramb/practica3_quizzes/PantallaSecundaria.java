8400e13e6972db3582db7f9f65ae467021f7a0f6
LauraMollon <lmollonb@iesch.org>
2021-10-23 12:33:40 +0200

    Aplicacion quizzes creada

---
+++ 1Trimestre/Practica3_Quizzes/app/src/main/java/com/lauramb/practica3_quizzes/PantallaSecundaria.java
@@ -0,0 +1,14 @@
+package com.lauramb.practica3_quizzes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaSecundaria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_secundaria);
+    }
+}
---
+++ 1Trimestre/Practica3_Quizzes/app/src/main/java/com/lauramb/practica3_quizzes/Pregunta.java
@@ -0,0 +1,2 @@
+package com.lauramb.practica3_quizzes;public class Pregunta {
+}
