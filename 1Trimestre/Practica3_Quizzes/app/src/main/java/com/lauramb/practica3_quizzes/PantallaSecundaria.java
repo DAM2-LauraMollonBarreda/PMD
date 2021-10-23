e230706e1b86512bc73ad9e4568f05c24b61aede
LauraMollon <lmollonb@iesch.org>
2021-10-23 12:36:15 +0200

    quizzes acabada

--- 1Trimestre/Practica3_Quizzes/app/src/main/java/com/lauramb/practica3_quizzes/PantallaSecundaria.java
+++ 1Trimestre/Practica3_Quizzes/app/src/main/java/com/lauramb/practica3_quizzes/PantallaSecundaria.java
@@ -2,13 +2,39 @@ package com.lauramb.practica3_quizzes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PantallaSecundaria extends AppCompatActivity {
+        TextView texto;
+        Button boton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pantalla_secundaria);
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            setTitle("Quizzes");
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_pantalla_secundaria);
+
+            texto= (TextView) findViewById(R.id.txtvTexto);
+            boton=(Button)findViewById(R.id.btnBoton);
+
+            Bundle extras = getIntent().getExtras();
+            texto.setText(extras.getString("Resultado correcto"));
+            boton.setText(extras.getString("Boton"));
+            Boolean esUltima = extras.getBoolean("Ultima");
+
+
+            boton.setOnClickListener(view -> {
+
+
+                if(boton.getText().equals("Siguiente")){
+                    super.onBackPressed();
+                }else{
+                    Intent nuevo = new Intent(this,MainActivity.class);
+                    startActivity(nuevo);
+                }
+            });
+        }
 }
