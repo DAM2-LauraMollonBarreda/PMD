a8d224c501a45325b1628393c969531f041fb653
damA <lmollonb@iesch.org>
2021-12-03 16:41:50 +0200

    pro

--- Proyecto1LauraMollon/app/src/main/java/com/laura/panaderia/MainActivity.java
+++ Proyecto1LauraMollon/app/src/main/java/com/laura/panaderia/MainActivity.java
@@ -2,13 +2,23 @@ package com.laura.panaderia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    RelativeLayout rellLocalizacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rellLocalizacion= findViewById(R.id.rellLocalizacion);
+
+        rellLocalizacion.setOnClickListener(view -> {
+            //Intent intent = new Intent(this)
+        });
     }
 }
--- Proyecto1LauraMollon/app/src/main/java/com/laura/panaderia/Splash/Splash.java
+++ Proyecto1LauraMollon/app/src/main/java/com/laura/panaderia/Splash/Splash.java
@@ -2,7 +2,12 @@ package com.laura.panaderia.Splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.laura.panaderia.MainActivity;
+import com.laura.panaderia.R;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Eliminamos la barra de arriba
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
