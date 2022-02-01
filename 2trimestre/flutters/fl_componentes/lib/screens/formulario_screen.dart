f6d6908fc6b948cf9b62df5aec12a4c13d2f0d27
damA <lmollonb@iesch.org>
2022-02-01 17:11:57 +0100

    01/02/22

--- 2trimestre/flutters/fl_componentes/lib/screens/formulario_screen.dart
+++ 2trimestre/flutters/fl_componentes/lib/screens/formulario_screen.dart
@@ -1,8 +1,21 @@
+import 'package:fl_componentes/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class FormularioScreen extends StatelessWidget {
+
+  //Necesito usar las keys
+  // Las keus son usadas para mantener la referencia al widget y voy
+  // a asignar una key al form
+  final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+  final Map<String, String> formvalues = {
+    'nombre' : 'Laura',
+    'correo' : 'lmollonb@iesch.org',
+    'passwd' : '123456',
+    'movil' : '630830881',
+  };
    
-  const FormularioScreen({Key? key}) : super(key: key);
+  FormularioScreen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
@@ -14,23 +27,48 @@ class FormularioScreen extends StatelessWidget {
         
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-          child: Column(
-            children: [
-              TextFormField(
-                autofocus: true,
-                textCapitalization: TextCapitalization.words,
-                onChanged: (value) {
-                  print('Valor: $value');
-                },
-                validator: (value){
-                  if(value=='') return 'Este campo es requerido';
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-              )
-            ],
+          child: Form(
+            key: myFormKey,
+            child: Column(
+              children: [
+                CustomInputField(formProperty: 'nombre',formValue: formvalues,hintText: 'Nombre usuario', labelText: 'Nombre', helperText: 'Introduce el nombre',sufixicon: Icons.person_add, icon: Icons.verified_user_rounded ),
+                const SizedBox(height: 30,),
+                 CustomInputField(formProperty: 'correo', formValue: formvalues, hintText: 'example@gmail.com', labelText: 'Correo',sufixicon: Icons.mail, teclado: TextInputType.emailAddress,),
+                const SizedBox(height: 30,),
+                CustomInputField(formProperty: 'passwd', formValue: formvalues,hintText: 'Contraseña del usuario', labelText: 'Contraseña',sufixicon: Icons.password,contrasena: true,),
+                const SizedBox(height: 30),
+                CustomInputField(formProperty: 'movil', formValue: formvalues,hintText: 'Telefono movil', labelText: 'Telefono',sufixicon: Icons.phone, teclado: TextInputType.phone,),
+                const SizedBox(height: 30,),
+                ElevatedButton(
+                  onPressed: (){
+                    //Quitar el teclado de la pantalla
+                    FocusScope.of(context).requestFocus(FocusNode());
+
+                    if (!myFormKey.currentState!.validate()){
+                      print('Formulario no valido');
+                      return;
+                    } 
+                      
+                    
+            
+                    //print(myFormKey.currentState.validate());
+                  }, 
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center(
+                      child: Text('Guardar'),
+                    ),
+                  )
+                )
+          
+          
+                
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
