e2446a6ad60eed08ff1473d42a7fc84fe6ddd331
damA <lmollonb@iesch.org>
2022-02-04 15:56:17 +0100

    cambios practica

--- 2trimestre/flutters/fl_componentes/lib/screens/navigationbar_screen.dart
+++ 2trimestre/flutters/fl_componentes/lib/screens/navigationbar_screen.dart
@@ -1,8 +1,14 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class NavigationBarScreen extends StatelessWidget {
+class NavigationBarScreen extends StatefulWidget {
+  @override
+  State<NavigationBarScreen> createState() => _NavigationBarScreenState();
+}
+
+class _NavigationBarScreenState extends State<NavigationBarScreen> {
   int _page = 0;
+
   GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
   @override
@@ -10,16 +16,28 @@ class NavigationBarScreen extends StatelessWidget {
     return  Scaffold(
       bottomNavigationBar: CurvedNavigationBar(
         key: _bottomNavigationKey,
-        items:<Widget> [
-          Icon(Icons.home,size: 30,),
+        items: [
           Icon(Icons.list,size: 30,),
-          Icon(Icons.card_travel,size: 30,)
+          Icon(Icons.home,size: 30,),
+          Icon(Icons.card_travel,size: 30,),
         ],
+        
         onTap: (value) {
-          
+          setState(() {
+            _page = value;
+          });
         },
       ),
-      body:Container(color: Colors.amber,)
+      body: Container(
+          color: Colors.amber,
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text(_page.toString(), textScaleFactor: 10.0),
+              ],
+            ),
+          ),
+        )
     );
   }
 }
